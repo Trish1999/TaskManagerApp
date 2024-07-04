1f2dbcee3759bdcccbb947e3fedbb37276f0d5bd
Trish1999 <trishnarani28@gmail.com>
2024-07-04 13:03:51 +0530

    created public page

--- client/src/pages/publicPage/PublicPage.jsx
+++ client/src/pages/publicPage/PublicPage.jsx
@@ -1,16 +1,71 @@
-import React from 'react'
+import React,{useEffect,useState} from 'react'
 import { useParams } from 'react-router-dom';
 
 import styles from "../../pages/publicPage/PublicPage.module.css"
 import { getTaskDetailsById } from '../../apis/TaskApi';
+import logo from "../../assets/Group 1.svg"
+import { FormatDate2 } from '../../components/FormatDate';
  
-    function PublicPage() {
-        const { taskId } = useParams();
+export default function PublicPage() {
+    const [taskData, setTaskData] = useState({});
+    const {taskId}  = useParams();
+    const { id, title, assignedTo, priority, category, dueDate, checklistItems=[] } = taskData;
+    useEffect(() => {
+        fetchTaskDetails();
+    }, [taskId]);
         
-  return (
-    <div>
-    </div>
-  )
-}
+    const fetchTaskDetails = async () => {
+        const result = await getTaskDetailsById(taskId);
+        setTaskData(result?.data || {})
+    };
+    console.log(priority)
+    const renderPriorityDot = () => {
+        let color;
+        switch (priority) {
+            case 'high':
+                color = '#ea0a0a';
+                break;
+            case 'moderate':
+                color = '#4683e6';
+                break;
+            case 'low':
+                color = 'rgba(21, 191, 61, 0.94)';
+                break;
+        }
 
-export default PublicPage
+
+              
+        return <div className={styles.priority} ><span className={styles.priorityDot} style={{ backgroundColor: color }} ></span>{priority} Priority</div>
+        }; 
+        
+    return (
+        <div >
+            <div className={styles.header}>
+                <img src={logo} alt="logo"></img>
+                </div>
+                <div className={styles.card}>
+                    <div className={priority}>
+                        {renderPriorityDot()}</div>
+                    <div className={styles.cardTitle}>{title}
+                    </div>
+                    <div className={styles.checklistheader}>
+                        <p>Checklist ({checklistItems.filter(item => item.isChecked).length}/{checklistItems.length})</p>
+                    </div>
+                    <div className={styles.checklist}>
+                    {(checklistItems).map((item, index) => (
+                        <div key={index} className={styles.checklistItem}>
+                            <input type="checkbox" id={`item-${index}`} checked={item.isChecked} readOnly style={{ marginRight: "0.7rem" }} />
+                            <label htmlFor={`item-${index}`}>{item.text}</label>
+                        </div>
+                     ))}
+                    </div>
+                <div className={styles.footer}>
+                    {dueDate ? <>Due Date
+                     <button className={styles.duedate} >{FormatDate2(dueDate)}</button></> : ""
+                    }
+                </div>
+                </div>
+            </div>
+    )
+}
+    
--- server/routes/Task.js
+++ server/routes/Task.js
@@ -6,7 +6,7 @@ const verifyToken = require("../middleware/verifyToken");
 
 router.post("/create", verifyToken, TaskController.createTask);
 router.delete("/delete/:taskId", verifyToken, TaskController.deleteTask);
-router.get("/task-details/:taskid", TaskController.getTaskDetailsById);
+router.get("/task-details/:taskId", TaskController.getTaskDetailsById);
 router.put("/update/:taskId", TaskController.updateTaskDetailsById);
 router.put("/update/category/:taskId", TaskController.updateCategoryById);
 router.put("/update/assignee/:taskId", TaskController.updateAssignee);
