98830c8c99db88d0531eb77ddd04ce3b25b995d1
Trish1999 <trishnarani28@gmail.com>
2024-07-04 14:08:37 +0530

    minor changes

--- client/src/apis/TaskApi.jsx
+++ client/src/apis/TaskApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Navigate } from "react-router-dom";
 
-const backendUrl = ` http://task-manager-appvercelapp-trish1999s-projects.vercel.app/api/v1/task`;
+const backendUrl = ` https://task-manager-appvercelapp-trish1999s-projects.vercel.app/api/v1/task`;
 
 export const createTask = async (postPayload) => {
     try {
--- client/src/apis/UserApi.jsx
+++ client/src/apis/UserApi.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import "react-toastify/dist/ReactToastify.css";
 
-const backendUrl = ` http://task-manager-appvercelapp-trish1999s-projects.vercel.app/api/v1/user`;
+const backendUrl = ` https://task-manager-appvercelapp-trish1999s-projects.vercel.app/api/v1/user`;
 
 export const registerUser = async ({ name,email, password }) => {
     try {
